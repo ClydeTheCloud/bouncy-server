f77aa4aea853b08baf80c2589dd7942d1b76c95d
Dke <evapromshelko@mail.ru>
2021-03-16 13:34:57 +0300

    game client is now hosted on server, removed CORS

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
+const staticRouter = express.Router()
+const path = require('path')
 
 const config = require('./utils/config')
 const scoreRouter = require('./controllers/scores')
 const middleware = require('./utils/middleware')
+
+staticRouter.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/index.html'))
+})
 
 console.log('connecting to DB')
 mongoose
@@ -17,9 +22,9 @@ mongoose
     .then(() => console.log('connected'))
     .catch(err => console.log('failed to connect', err))
 
-app.use(cors())
 app.use(express.json())
 app.use('/score/', scoreRouter)
+app.use(express.static('static'))
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
